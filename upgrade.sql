938835eae5da3ff52a5ad4cde60e078610e07ffa
Mohamed Hassime NASRI <Mohamed.Hassime.NASRI@bnpparibas.com>
2019-04-04 18:42:45 +0200

    Optimisation de la BDD

--- upgrade.sql
+++ upgrade.sql
@@ -1,53 +1,72 @@
--- Sql - table users
+-- Sql - table addresses
 
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `login` varchar(225) NOT NULL,
-  `email` varchar(225) NOT NULL,
-  `password` varchar(225) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+DROP TABLE IF EXISTS `addresses`;
+CREATE TABLE IF NOT EXISTS `addresses`
+(
+    `id`         int(11)      NOT NULL AUTO_INCREMENT,
+    `number`     int(4)       NOT NULL,
+    `street`     varchar(225) NOT NULL,
+    `postalCode` int(6)       NOT NULL,
+    `city`       varchar(225) NOT NULL,
+    `country`    varchar(225) NOT NULL,
+    `idContact`  int(11)      NOT NULL,
+    PRIMARY KEY (`id`),
+    KEY `idContact` (`idContact`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+
 
 -- Sql - table contacts
 
 DROP TABLE IF EXISTS `contacts`;
-CREATE TABLE IF NOT EXISTS `contacts` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nom` varchar(225) NOT NULL,
-  `prenom` varchar(225) NOT NULL,
-  `email` varchar(225) NOT NULL,
-  `userId` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `userId` (`userId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE IF NOT EXISTS `contacts`
+(
+    `id`        int(11)      NOT NULL AUTO_INCREMENT,
+    `lastname`  varchar(50)  NOT NULL,
+    `firstname` varchar(50)  NOT NULL,
+    `email`     varchar(225) NOT NULL,
+    `userId`    int(11)      NOT NULL,
+    PRIMARY KEY (`id`),
+    KEY `userId` (`userId`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
--- Sql - table addresses
 
-DROP TABLE IF EXISTS `addresses`;
-CREATE TABLE IF NOT EXISTS `addresses` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `number` int(4) NOT NULL,
-  `street` varchar(225) NOT NULL,
-  `postalCode` int(6) NOT NULL,
-  `city` varchar(225) NOT NULL,
-  `country` varchar(225) NOT NULL,
-  `idContact` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `idContact` (`idContact`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- Sql - table users
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users`
+(
+    `id`       int(11)      NOT NULL AUTO_INCREMENT,
+    `login`    varchar(50)  NOT NULL,
+    `email`    varchar(225) NOT NULL,
+    `password` varchar(225) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+
 
 -- Sql - contraintes
 
 ALTER TABLE `addresses`
-  ADD CONSTRAINT `addresses_ibfk_1` FOREIGN KEY (`idContact`) REFERENCES `contacts` (`id`) ON DELETE CASCADE;
+    ADD CONSTRAINT `addresses_ibfk_1` FOREIGN KEY (`idContact`) REFERENCES `contacts` (`id`) ON DELETE CASCADE;
 
 ALTER TABLE `contacts`
-  ADD CONSTRAINT `contacts_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+    ADD CONSTRAINT `contacts_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 COMMIT;
+
 
 -- Sql - initialisation user de test
 
-INSERT INTO `users` (`id`, `login`, `email`, `password`) VALUES
-(1, 'admin', 'lebonoin@test.fr', '21232f297a57a5a743894a0e4a801fc3');
+INSERT INTO `users` (`id`, `login`, `email`, `password`)
+VALUES (1, 'admin', 'lebonoin@admin.fr', '21232f297a57a5a743894a0e4a801fc3');
+
+INSERT INTO `users` (`id`, `login`, `email`, `password`)
+VALUES (2, 'test', 'test@test.fr', '21232f297a57a5a743894a0e4a801fc3');
 COMMIT;
+
+
+-- Sql - Ajout du fulltext index sur nom et prenom
+
+ALTER TABLE contacts
+    ADD FULLTEXT (lastname, firstname)
